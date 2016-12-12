d99bdc6eeaf343acc47d79043cfb7613b1c510ef
chenshenqian <chenshenqian@meituan.com>
2016-12-12 18:10:18 +0800

    change installing approach

--- git-hook/git-hook-install.sh
+++ git-hook/git-hook-install.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 INSTALL_PATH="$(cd "$(dirname "$0")" && pwd -P)"
-PROJECT_ROOT=${PROJECT_ROOT:-$(cd "$INSTALL_PATH/../../.."; pwd -P)}
+GIT_ROOT_DIR="$(git rev-parse --show-toplevel || echo "$INSTALL_PATH/../../..")"
+PROJECT_ROOT=${PROJECT_ROOT:-$(cd "$GIT_ROOT_DIR"; pwd -P)}
 FROM_HOOK_PATH="$INSTALL_PATH/hooks"
 TO_HOOK_PATH="$PROJECT_ROOT/.git/hooks"
-HOOK_FILE_NAMES=$(ls ${FROM_HOOK_PATH})
-
-_CURRENT_DIR=$(pwd -P)
-cd "$PROJECT_ROOT"
-while [ ! -d ".git" ]; do
-  cd ..
-done
-GIT_HOOK_PATH="$(pwd -P)/.git/hooks"
-cd "$_CURRENT_DIR"
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+AFFECT_HOOKFILES=("prepare-commit-msg" "pre-push" "commit-msg")
 
 is_node_env_dev() {
   node_env=$1
@@ -33,44 +27,45 @@ has_git_hooks_path() {
 is_node_env_dev $NODE_ENV
 has_git_hooks_path $TO_HOOK_PATH
 
-for hook_file in ${HOOK_FILE_NAMES}
+for hook_file in "${AFFECT_HOOKFILES[@]}"
 do
-  ALL_HOOKS+=($hook_file)
-  if [ -f "$TO_HOOK_PATH/$hook_file" ]; then
-    file_diff=$(diff "$FROM_HOOK_PATH/$hook_file" "$TO_HOOK_PATH/$hook_file")
-    
-    if [ -z "$file_diff" ]; then
-      EXIST_HOOKS+=(${hook_file})
+  if [ -f "$TO_HOOK_PATH/$hook_file" ]
+  then
+    if [ -f "$FROM_HOOK_PATH/$hook_file" ]
+    then
+      diff_info=$(diff "$FROM_HOOK_PATH/$hook_file" "$TO_HOOK_PATH/$hook_file")
+      if [ ! -z "${diff_info}" ]
+      then
+        INSTALL_HOOKS+=(${hook_file})
+        EXIST_HOOKS+=(${hook_file})
+      fi
     else
-      UPDATE_HOOKS+=(${hook_file})
+      EXIST_HOOKS+=(${hook_file})
     fi
-  else
+  elif [ -f "$FROM_HOOK_PATH/$hook_file" ]
+  then
     INSTALL_HOOKS+=(${hook_file})
   fi
 done
+  
+echo -e "GIT LOCAL HOOK installing...! ⚙ \n"
 
-if [ ${#EXIST_HOOKS[@]} -eq ${#ALL_HOOKS[@]} ]; then
-  echo "No git hook need to update or install."
-else
-  echo -e "GIT LOCAL HOOK installing...! ⚙ \n"
-  if [ ${#UPDATE_HOOKS[@]} -gt 0 ]; then
-    for hook_file in ${UPDATE_HOOKS[@]}
-    do
-      file_diff=$(diff "$FROM_HOOK_PATH/$hook_file" "$TO_HOOK_PATH/$hook_file")
-      echo -e "${hook_file} has changed: \n${file_diff}"
-      echo "${hook_file} updating..."
-      cp "$FROM_HOOK_PATH/$hook_file" "$TO_HOOK_PATH/$hook_file"
-      echo -e "${hook_file} updated!\n"
-     done
-  fi
-  if [ ${#INSTALL_HOOKS[@]} -gt 0 ]; then
-    for hook_file in ${INSTALL_HOOKS[@]}
-    do
-      echo "${hook_file} installing..."
-      cp "$FROM_HOOK_PATH/$hook_file" "$TO_HOOK_PATH/$hook_file"
-      echo -e "${hook_file} installed!\n"
-     done
-  fi
-  echo "GIT LOCAL HOOK install done!  🍻"
-fi 
+if [ ${#EXIST_HOOKS[@]} -gt 0 ]
+then
+  for hook_file in ${EXIST_HOOKS[@]}
+  do
+    echo "bak up ${hook_file}"
+    mv "$TO_HOOK_PATH/$hook_file" "$TO_HOOK_PATH/$hook_file.$TIMESTAMP"
+  done
+fi
 
+if [ ${#INSTALL_HOOKS[@]} -gt 0 ]
+then
+  for hook_file in ${INSTALL_HOOKS[@]}
+  do
+    echo "install ${hook_file}"
+    cp "$FROM_HOOK_PATH/$hook_file" "$TO_HOOK_PATH/$hook_file"
+  done
+fi
+
+echo "GIT LOCAL HOOK install done!  🍻 "
